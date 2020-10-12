446b4c3403e05ba897c487d6e60969a21b298db5
Tanmay Jain <mail.tanmay.jain@gmail.com>
2020-10-12 16:16:19 +0530

    Add: Binary Tree Node def. Class

---
+++ main/java/bridgelabz/hashTable_binaryTrees/BinaryTreeNode.java
@@ -0,0 +1,20 @@
+package bridgelabz.hashTable_binaryTrees;
+
+public class BinaryTreeNode<K extends Comparable<K>> {
+
+	//Taking class vars
+	K key;
+	BinaryTreeNode<K> leftBranch;
+	BinaryTreeNode<K> rightBranch;
+
+	/**Constructor of BinaryTreeNode
+	 * @param Key value of TreeNode
+	 */
+	public BinaryTreeNode(K key) {
+		this.key = key;
+		this.leftBranch = null;
+		this.rightBranch = null;
+	}
+
+
+}
